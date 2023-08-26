01d42da124654841b185541c614cb63db968d075
Steven51516 <sliu0727@usc.edu>
2023-08-26 14:42:33 -0700

    env

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ from BioEncoder.task.DTI import DTITrainer
 drugs, proteins, labels = load_DTI("data/DAVIS/data.txt")
 drug_encoder = DrugEncoder("CNN")
 target_encoder = ProteinEncoder("CNN")
-
 trainer = DTITrainer(drug_encoder, target_encoder)
 train, val, test = trainer.prepare_datasets(drugs, proteins, labels, [0.7, 0.2, 0.1])
 trainer.train(train, val)
